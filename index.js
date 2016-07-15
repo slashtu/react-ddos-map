9e093e2d1a19a29730e4b1f51a17de45c78de0ca
Slash <y12341234@gmail.com>
2016-07-15 16:28:45 +0800

    install testing

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = {
-    sayName: function(){console.log('my name is slash');}
-}
+'use strict';
+
+var React = require('react');
+
+var DDos = React.createClass({
+  render: function(){
+    return (
+      React.createElement('div', {id: 'map'}, 'This is a map')
+    );
+  }
+})
+
+
+module.exports = DDos;
